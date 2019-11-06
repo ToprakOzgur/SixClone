d7b3fd6b55793fd2b309fc9dbc226e10c05bf1f3
ToprakOzgur <zgrtprk@gmail.com>
2019-11-06 09:34:32 +0300

    new prefabs

---
+++ Assets/_Scripts/Block.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public Size size;
+
+    [System.Serializable]
+    public struct Size
+    {
+        public int width;
+        public int height;
+    }
+}
