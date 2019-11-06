7a606b05f29aba249afc552b715f8d2a053ab8f5
ToprakOzgur <zgrtprk@gmail.com>
2019-11-06 12:14:24 +0300

    tiles added to shapes

---
+++ Assets/_Scripts/BlockGroup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGroup
+{
+    public int height { get; private set; }
+    private const int width = 5;
+
+    private int[,] grid;
+    public BlockGroup(int height)
+    {
+        this.height = height;
+        grid = new int[width, height];
+
+    }
+
+    public void GenerateBlockGroup(Vector2 spawnPoint, Block[] blocks)
+    {
+
+    }
+
+
+}
--- Assets/_Scripts/Tile.cs
+++ Assets/_Scripts/Tile.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block : MonoBehaviour
+public class Tile : MonoBehaviour
 {
-    public Tile[] tiles;
+
 }
